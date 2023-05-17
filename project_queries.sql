1b9a556f33941c98a73c4bee293a27ff49ce8c05
f87-56 <45468114+f87-56@users.noreply.github.com>
2023-05-17 18:50:43 +0300

    Fixed issue

--- project_queries.sql
+++ project_queries.sql
@@ -376,7 +376,7 @@ INSERT INTO ExerciseGroup (courseStartDate, courseCode, groupName, maxAttendees)
     ('2023-01-09', 'LC-1101', 'Group E', 50),
     ('2023-09-01', 'CS-101', 'Group F', 50),
     ('2023-09-01', 'MS-201', 'Group G', 50),
-    ('2023-09-01', 'MS-201', 'Group S',48); -- for removal
+    ('2023-09-01', 'LC-1101', 'Group S',48); -- for removal
     
 INSERT INTO Hall (hallName, buildingName, seats, maxExaminees) VALUES
     ('Auditorium', 'Computer Science building', 500, 250),
